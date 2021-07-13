daf932880a5f19ac4a2a467753b234b8d644f677
hkrsmk <lijiayangsg@gmail.com>
2021-07-14 01:53:17 +0800

    cleaned data to become json format

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# IDScraper
+# IDScraper folder
 In the below website, in status of charity, select 'IPCs' and click 'Search'
 https://www.charities.gov.sg/Pages/AdvanceSearch.aspx?q=
 
@@ -9,5 +9,8 @@ Observe the query and then write your own in cURL (see pictures and .out file)
 
 If using the easier method, `CharityAccountCRMRecordID` should be encoded with base64 before putting in as a query for individual charities.
 
-# curlscrape.sh
-Uses curl in a bash script to get all the info of each individual charity - Profile, Financial Info, Annual Report, Code Compliance. This is all stored in the `orginfo.txt` file. The `orginfo.txt` file will be converted into an excel sheet with just the relevant info - a short description of the charity, Income vs Expenditure (for room for funding), and any other info for evaluating effectiveness.
+# datascraper folder
+Uses curl in a bash script `curlscrape.sh` to get all the info of each individual charity - Profile, Financial Info, Annual Report, Code Compliance. This is all stored in the `orginfo.txt` file. The `orginfo.txt` file will be converted into an excel sheet with just the relevant info - a short description of the charity, Income vs Expenditure (for room for funding), and any other info for evaluating effectiveness.
+
+# dataclean.py
+The txt files were renamed to json as `orginfo.json`, and remove the excessive commands and keep only the information we want. Now it should be a proper json file. Trailing commas had to be removed manually; keeping the one in `orginfosamplejsonmake.json` to make it obvious.
---
+++ dataclean.py
@@ -0,0 +1,47 @@
+import re
+
+def uncurl(file_to_edit):
+    '''remove curl commands and empty lines from the appended curl output, overwriting the final file each time
+    taken from https://stackoverflow.com/questions/11968998/remove-lines-that-contain-certain-string
+    https://stackoverflow.com/questions/3711856/how-to-remove-empty-lines-with-or-without-whitespace-in-python
+    '''
+
+    bad_words = ['--_curl_--https:', '&type=']
+
+    with open(file_to_edit+'.json') as oldfile, open(file_to_edit+'new.json', 'w+') as newfile:
+
+        # new content with only non-curl lines written, and non-empty lines
+        for line in oldfile:
+            if not any(bad_word in line for bad_word in bad_words) and not re.match(r'^\s*$', line):
+                newfile.write(line)
+
+    return
+
+def json_maker(file):
+    '''change the json object into an array by adding '[' ']' and also the relevant commas
+    https://stackoverflow.com/questions/6791468/array-of-json-objects
+
+    However, we have to remove the last trailing comma by hand.
+
+    uncomment the code if the data is multi-line
+    '''
+
+    file_to_filter = open(file + 'new.json', 'r')
+
+    with open(file + 'jsonmake.json', 'w') as jsonified_file:
+        jsonified_file.write('[')
+
+        for line in file_to_filter:
+            # if line == '}\n':
+                # jsonified_file.write(line + ',')
+            # else:
+                jsonified_file.write(line + ',')
+
+        jsonified_file.write(']')
+
+    return
+
+# uncurl('orginfosample')
+# json_maker('orginfosample')
+# uncurl('datascraper/orginfo')
+# json_maker('datascraper/orginfo')
