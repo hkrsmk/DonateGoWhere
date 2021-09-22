17ff0a6eb2c4729594682952218a3efa6f31214d
whyexis <chong.ying.xiang@gmail.com>
2021-09-22 23:39:03 +0800

    Initial navbar component

---
+++ app/src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>DonateGoWhere</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/evaluate">Evaluate</a>
+        <a href="/contact">Contact</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
